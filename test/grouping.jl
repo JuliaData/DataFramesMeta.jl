0ab54199ced5fd179ef6afcf6c9bac39846f185b
Tom Short <tshort.rlists@gmail.com>
2014-11-17 18:39:38 -0500

    Fix tests (still some deprecieated warnings from DataFrame).

--- test/grouping.jl
+++ test/grouping.jl
@@ -6,18 +6,17 @@ using Base.Test
 using DataArrays, DataFrames
 using DataFramesMeta
 
-srand(1)
-d = DataFrame(n = 1:20, x = rand(1:3, 20))
+d = DataFrame(n = 1:20, x = [3, 3, 3, 3, 1, 1, 1, 2, 1, 1, 2, 1, 1, 2, 2, 2, 3, 1, 1, 2])
 g = groupby(d, :x)
 
 @test  @where(d, :x .== 3) == where(d, x -> x[:x] .== 3)
 @test  DataFrame(@where(g, length(:x) > 5)) == DataFrame(where(g, x -> length(x[:x]) > 5))
-@test  DataFrame(@where(g, length(:x) > 5))[:n][1:9] == @data [3, 5, 12, 14, 17, 19, 1, 2, 4]
+@test  DataFrame(@where(g, length(:x) > 5))[:n][1:3] == @data [5, 6, 7]
 
 @test  DataFrame(orderby(g, x -> mean(x[:n]))) == DataFrame(@orderby(g, mean(:n)))
-@test  DataFrames.based_on(@orderby(g, mean(:n)), x -> x[1,:x])[:x1] == [2,1,3]
+@test  DataFrames.based_on(@orderby(g, mean(:n)), x -> x[1,:x])[:x1] == [3,1,2]
 
-@test  (@transform(g, y = :n - median(:n)))[1,:y] == -10.0
+@test  (@transform(g, y = :n - median(:n)))[1,:y] == -5.0
 
 
 end # module
