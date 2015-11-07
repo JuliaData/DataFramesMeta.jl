b8197415d2e30c72eb0c6627737594d04d18fc8c
Tom Short <tshort.rlists@gmail.com>
2015-11-07 10:16:44 -0500

    Add a test for the linq version of where with multiple args

--- test/linqmacro.jl
+++ test/linqmacro.jl
@@ -11,19 +11,19 @@ df = DataFrame(a = rand(1:3, n),
                b = ["a","b","c","d"][rand(1:4, n)],
                x = rand(n))
 
-x = @where(df, :a .> 2)
+x = @where(df, :a .> 2, :b .!= "c")
 x = @transform(x, y = 10 * :x)
 x = @by(x, :b, meanX = mean(:x), meanY = mean(:y))
 x = @orderby(x, :b, -:meanX)
 x = @select(x, var = :b, :meanX, :meanY)
 
-x1 = @linq transform(where(df, :a .> 2), y = 10 * :x)
+x1 = @linq transform(where(df, :a .> 2, :b .!= "c"), y = 10 * :x)
 x1 = @linq by(x1, :b, meanX = mean(:x), meanY = mean(:y))
 x1 = @linq select(orderby(x1, :b, -:meanX), var = :b, :meanX, :meanY)
 
 ## chaining
 xlinq = @linq df  |>
-    where(:a .> 2)  |>
+    where(:a .> 2, :b .!= "c")  |>
     transform(y = 10 * :x)  |>
     by(:b, meanX = mean(:x), meanY = mean(:y))  |>
     orderby(:b, -:meanX)  |>
