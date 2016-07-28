1cd29d7f731c2d1271e9b8910a0ab2f688396cab
Alex Arslan <ararslan@comcast.net>
2016-07-28 14:19:17 -0700

    Use Tom Short's updated LINQ test

--- test/linqmacro.jl
+++ test/linqmacro.jl
@@ -1,11 +1,10 @@
-
 module TestLinqMacro
 
 using Base.Test
 using DataArrays, DataFrames
 using DataFramesMeta
 
-srand(1)
+srand(100)
 n = 100
 df = DataFrame(a = rand(1:3, n),
                b = ["a","b","c","d"][rand(1:4, n)],
@@ -14,19 +13,19 @@ df = DataFrame(a = rand(1:3, n),
 x = @where(df, :a .> 2, :b .!= "c")
 x = @transform(x, y = 10 * :x)
 x = @by(x, :b, meanX = mean(:x), meanY = mean(:y))
-x = @orderby(x, :b, -:meanX)
+x = @orderby(x, -:meanX)
 x = @select(x, var = :b, :meanX, :meanY)
 
 x1 = @linq transform(where(df, :a .> 2, :b .!= "c"), y = 10 * :x)
 x1 = @linq by(x1, :b, meanX = mean(:x), meanY = mean(:y))
-x1 = @linq select(orderby(x1, :b, -:meanX), var = :b, :meanX, :meanY)
+x1 = @linq select(orderby(x1, -:meanX), var = :b, :meanX, :meanY)
 
 ## chaining
 xlinq = @linq df  |>
     where(:a .> 2, :b .!= "c")  |>
     transform(y = 10 * :x)  |>
     by(:b, meanX = mean(:x), meanY = mean(:y))  |>
-    orderby(:b, -:meanX)  |>
+    orderby(-:meanX)  |>
     select(var = :b, :meanX, :meanY)
 
 @test x == x1
@@ -38,7 +37,7 @@ xlinq2 = @linq df  |>
     groupby(:b) |>
     orderby(-mean(:x))  |>
     based_on(meanX = mean(:x), meanY = mean(:y))
-    
+
 @test xlinq2[[:meanX, :meanY]] == xlinq[[:meanX, :meanY]]
 
 end
