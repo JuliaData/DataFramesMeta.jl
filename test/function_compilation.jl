c4d78d6a1093a92c6cfd3d1e368f8e9763e1814a
Peter Deffebach <p.deffebach@gmail.com>
2021-03-29 20:04:51 -0400

    fix tests

--- test/function_compilation.jl
+++ test/function_compilation.jl
@@ -5,153 +5,157 @@ using DataFramesMeta
 
 @testset "function_compilation" begin
 	# Lazy way of making sure all
-	# functions are pre-compiled.
+	# functions are pre-compiled. @eval
+	# prevents julia from caching the
+	# intermediate anonymous functions.
 	for _ in 1:2
-		df = DataFrame(a = [1], b = [2])
+		@eval begin
+			df = DataFrame(a = [1], b = [2])
 
-		testfun(x, y) = x .* y
-		testdotfun(x, y) = x * y
-		testnt(x) = (c = x,)
+			testfun(x, y) = x .* y
+			testdotfun(x, y) = x * y
+			testnt(x) = (c = x,)
 
-		@test @select(df, c = :a + :b) == DataFrame(c = [3])
+			@test @select(df, c = :a + :b) == DataFrame(c = [3])
 
-			fasttime = @timed @select(df, c = :a + :b)
-			slowtime = @timed select(df, [:a, :b] => ((a, b) -> a + b) => :c)
-			@test slowtime.time > fasttime.time
+				fasttime = @timed @select(df, c = :a + :b)
+				slowtime = @timed select(df, [:a, :b] => ((a, b) -> a + b) => :c)
+				@test slowtime[2] > fasttime[2]
 
-		@test @select(df, cols(:c) = :a + :b) == DataFrame(c = [3])
+			@test @select(df, cols(:c) = :a + :b) == DataFrame(c = [3])
 
-			fasttime = @timed @select(df, cols(:c) = :a + :b)
-			slowtime = @timed select(df, [:a, :b] => ((a, b) -> a + b) => :c)
-			@test slowtime.time > fasttime.time
+				fasttime = @timed @select(df, cols(:c) = :a + :b)
+				slowtime = @timed select(df, [:a, :b] => ((a, b) -> a + b) => :c)
+				@test slowtime[2] > fasttime[2]
 
-		@test @select(df, c = :a .+ :b) == DataFrame(c = [3])
+			@test @select(df, c = :a .+ :b) == DataFrame(c = [3])
 
-			fasttime = @timed @select(df, c = :a .+ :b)
-			slowtime = @timed select(df, [:a, :b] => ((a, b) -> a .+ b) => :c)
-			@test slowtime.time > fasttime.time
+				fasttime = @timed @select(df, c = :a .+ :b)
+				slowtime = @timed select(df, [:a, :b] => ((a, b) -> a .+ b) => :c)
+				@test slowtime[2] > fasttime[2]
 
-		@test @select(df, cols(:c) = :a .+ :b) == DataFrame(c = [3])
+			@test @select(df, cols(:c) = :a .+ :b) == DataFrame(c = [3])
 
-			fasttime = @timed @select(df, cols(:c) = :a .+ :b)
-			slowtime = @timed select(df, [:a, :b] => ((a, b) -> a .+ b) => :c)
-			@test slowtime.time > fasttime.time
+				fasttime = @timed @select(df, cols(:c) = :a .+ :b)
+				slowtime = @timed select(df, [:a, :b] => ((a, b) -> a .+ b) => :c)
+				@test slowtime[2] > fasttime[2]
 
-		@test @select(df, c = cols(:a) + cols(:b)) == DataFrame(c = [3])
+			@test @select(df, c = cols(:a) + cols(:b)) == DataFrame(c = [3])
 
-			fasttime = @timed @select(df, c = cols(:a) + cols(:b))
-			slowtime = @timed select(df, [:a, :b] => ((a, b) -> a + b) => :c)
-			@test slowtime.time > fasttime.time
+				fasttime = @timed @select(df, c = cols(:a) + cols(:b))
+				slowtime = @timed select(df, [:a, :b] => ((a, b) -> a + b) => :c)
+				@test slowtime[2] > fasttime[2]
 
-		@test @select(df, cols(:c) = cols(:a) + cols(:b)) == DataFrame(c = [3])
+			@test @select(df, cols(:c) = cols(:a) + cols(:b)) == DataFrame(c = [3])
 
-			fasttime = @timed @select(df, cols(:c) = cols(:a) + cols(:b))
-			slowtime = @timed select(df, [:a, :b] => ((a, b) -> a + b) => :c)
-			@test slowtime.time > fasttime.time
+				fasttime = @timed @select(df, cols(:c) = cols(:a) + cols(:b))
+				slowtime = @timed select(df, [:a, :b] => ((a, b) -> a + b) => :c)
+				@test slowtime[2] > fasttime[2]
 
-		@test @select(df, c = cols(:a) .+ cols(:b)) == DataFrame(c = [3])
+			@test @select(df, c = cols(:a) .+ cols(:b)) == DataFrame(c = [3])
 
-			fasttime = @timed @select(df, c = cols(:a) .+ cols(:b))
-			slowtime = @timed select(df, [:a, :b] => ((a, b) -> a .+ b) => :c)
-			@test slowtime.time > fasttime.time
+				fasttime = @timed @select(df, c = cols(:a) .+ cols(:b))
+				slowtime = @timed select(df, [:a, :b] => ((a, b) -> a .+ b) => :c)
+				@test slowtime[2] > fasttime[2]
 
-		@test @select(df, cols(:c) = cols(:a) .+ cols(:b)) == DataFrame(c = [3])
+			@test @select(df, cols(:c) = cols(:a) .+ cols(:b)) == DataFrame(c = [3])
 
-			fasttime = @timed @select(df, cols(:c) = cols(:a) .+ cols(:b))
-			slowtime = @timed select(df, [:a, :b] => ((a, b) -> a .+ b) => :c)
-			@test slowtime.time > fasttime.time
+				fasttime = @timed @select(df, cols(:c) = cols(:a) .+ cols(:b))
+				slowtime = @timed select(df, [:a, :b] => ((a, b) -> a .+ b) => :c)
+				@test slowtime[2] > fasttime[2]
 
-		@test @select(df, c = :a) == DataFrame(c = [1])
+			@test @select(df, c = :a) == DataFrame(c = [1])
 
-			fasttime = @timed @select(df, c = :a)
-			slowtime = @timed select(df, [:a] => (a -> a) => :c)
-			@test slowtime.time > fasttime.time
+				fasttime = @timed @select(df, c = :a)
+				slowtime = @timed select(df, [:a] => (a -> a) => :c)
+				@test slowtime[2] > fasttime[2]
 
-		@test @select(df, cols(:c) = :a) == DataFrame(c = [1])
+			@test @select(df, cols(:c) = :a) == DataFrame(c = [1])
 
-			fasttime = @timed @select(df, cols(:c) = :a)
-			slowtime = @timed select(df, [:a] => (a -> a) => :c)
-			@test slowtime.time > fasttime.time
+				fasttime = @timed @select(df, cols(:c) = :a)
+				slowtime = @timed select(df, [:a] => (a -> a) => :c)
+				@test slowtime[2] > fasttime[2]
 
-		@test @select(df, c = cols(:a)) == DataFrame(c = [1])
+			@test @select(df, c = cols(:a)) == DataFrame(c = [1])
 
-			fasttime = @timed @select(df, c = cols(:a))
-			slowtime = @timed select(df, [:a] => (a -> a) => :c)
-			@test slowtime.time > fasttime.time
+				fasttime = @timed @select(df, c = cols(:a))
+				slowtime = @timed select(df, [:a] => (a -> a) => :c)
+				@test slowtime[2] > fasttime[2]
 
-		@test @select(df, cols(:c) = cols(:a)) == DataFrame(c = [1])
+			@test @select(df, cols(:c) = cols(:a)) == DataFrame(c = [1])
 
-			fasttime = @timed @select(df, cols(:c) = cols(:a))
-			slowtime = @timed select(df, [:a] => (a -> a) => :c)
-			@test slowtime.time > fasttime.time
+				fasttime = @timed @select(df, cols(:c) = cols(:a))
+				slowtime = @timed select(df, [:a] => (a -> a) => :c)
+				@test slowtime[2] > fasttime[2]
 
-		@test @select(df, :a) == df[:, [:a]]
+			@test @select(df, :a) == df[:, [:a]]
 
-			fasttime = @timed @select(df, :a)
-			slowtime = @timed select(df, [:a] => (a -> a) => :a)
-			@test slowtime.time > fasttime.time
+				fasttime = @timed @select(df, :a)
+				slowtime = @timed select(df, [:a] => (a -> a) => :a)
+				@test slowtime[2] > fasttime[2]
 
-		@test @select(df, cols(:a)) == df[:, [:a]]
+			@test @select(df, cols(:a)) == df[:, [:a]]
 
-			fasttime = @timed @select(df, cols(:a))
-			slowtime = @timed select(df, [:a] => (a -> a) => :a)
-			@test slowtime.time > fasttime.time
+				fasttime = @timed @select(df, cols(:a))
+				slowtime = @timed select(df, [:a] => (a -> a) => :a)
+				@test slowtime[2] > fasttime[2]
 
-		@test @select(df, c = testfun(:a, :b)) == DataFrame(c = [2])
+			@test @select(df, c = testfun(:a, :b)) == DataFrame(c = [2])
 
-			fasttime = @timed @select(df, c = testfun(:a, :b))
-			slowtime = @timed select(df, [:a, :b] => ((a, b) -> testfun(a, b)) => :c)
-			@test slowtime.time > fasttime.time
+				fasttime = @timed @select(df, c = testfun(:a, :b))
+				slowtime = @timed select(df, [:a, :b] => ((a, b) -> testfun(a, b)) => :c)
+				@test slowtime[2] > fasttime[2]
 
-		@test @select(df, cols(:c) = testfun(:a, :b)) == DataFrame(c = [2])
+			@test @select(df, cols(:c) = testfun(:a, :b)) == DataFrame(c = [2])
 
-			fasttime = @timed @select(df, cols(:c) = testfun(:a, :b))
-			slowtime = @timed select(df, [:a, :b] => ((a, b) -> testfun(a, b)) => :c)
-			@test slowtime.time > fasttime.time
+				fasttime = @timed @select(df, cols(:c) = testfun(:a, :b))
+				slowtime = @timed select(df, [:a, :b] => ((a, b) -> testfun(a, b)) => :c)
+				@test slowtime[2] > fasttime[2]
 
-		@test @select(df, c = testfun(cols("a"), cols("b"))) == DataFrame(c = [2])
+			@test @select(df, c = testfun(cols("a"), cols("b"))) == DataFrame(c = [2])
 
-			fasttime = @timed @select(df, c = testfun(cols("a"), cols("b")))
-			slowtime = @timed select(df, [:a, :b] => ((a, b) -> testfun(a, b)) => :c)
-			@test slowtime.time > fasttime.time
+				fasttime = @timed @select(df, c = testfun(cols("a"), cols("b")))
+				slowtime = @timed select(df, [:a, :b] => ((a, b) -> testfun(a, b)) => :c)
+				@test slowtime[2] > fasttime[2]
 
-		@test @select(df, cols(:c) = testfun(cols("a"), cols("b"))) == DataFrame(c = [2])
+			@test @select(df, cols(:c) = testfun(cols("a"), cols("b"))) == DataFrame(c = [2])
 
-			fasttime = @timed @select(df, cols(:c) = testfun(cols("a"), cols("b")))
-			slowtime = @timed select(df, [:a, :b] => ((a, b) -> testfun(a, b)) => :c)
-			@test slowtime.time > fasttime.time
+				fasttime = @timed @select(df, cols(:c) = testfun(cols("a"), cols("b")))
+				slowtime = @timed select(df, [:a, :b] => ((a, b) -> testfun(a, b)) => :c)
+				@test slowtime[2] > fasttime[2]
 
-		@test @select(df, c = testdotfun.(:a, :b)) == DataFrame(c = [2])
+			@test @select(df, c = testdotfun.(:a, :b)) == DataFrame(c = [2])
 
-			fasttime = @timed @select(df, c = testdotfun.(:a, :b))
-			slowtime = @timed select(df, [:a, :b] => ((a, b) -> testdotfun.(a, b)) => :c)
-			@test slowtime.time > fasttime.time
+				fasttime = @timed @select(df, c = testdotfun.(:a, :b))
+				slowtime = @timed select(df, [:a, :b] => ((a, b) -> testdotfun.(a, b)) => :c)
+				@test slowtime[2] > fasttime[2]
 
-		@test @select(df, cols(:c) = testdotfun.(:a, :b)) == DataFrame(c = [2])
+			@test @select(df, cols(:c) = testdotfun.(:a, :b)) == DataFrame(c = [2])
 
-			fasttime = @timed @select(df, cols(:c) = testdotfun.(:a, :b))
-			slowtime = @timed select(df, [:a, :b] => ((a, b) -> testdotfun.(a, b)) => :c)
-			@test slowtime.time > fasttime.time
+				fasttime = @timed @select(df, cols(:c) = testdotfun.(:a, :b))
+				slowtime = @timed select(df, [:a, :b] => ((a, b) -> testdotfun.(a, b)) => :c)
+				@test slowtime[2] > fasttime[2]
 
-		@test @select(df, c = testdotfun.(cols("a"), cols("b"))) == DataFrame(c = [2])
+			@test @select(df, c = testdotfun.(cols("a"), cols("b"))) == DataFrame(c = [2])
 
-			fasttime = @timed @select(df, c = testdotfun.(cols("a"), cols("b")))
-			slowtime = @timed select(df, [:a, :b] => ((a, b) -> testdotfun.(a, b)) => :c)
-			@test slowtime.time > fasttime.time
+				fasttime = @timed @select(df, c = testdotfun.(cols("a"), cols("b")))
+				slowtime = @timed select(df, [:a, :b] => ((a, b) -> testdotfun.(a, b)) => :c)
+				@test slowtime[2] > fasttime[2]
 
-		@test @select(df, cols(:c) = testdotfun.(cols("a"), cols("b"))) == DataFrame(c = [2])
+			@test @select(df, cols(:c) = testdotfun.(cols("a"), cols("b"))) == DataFrame(c = [2])
 
-			fasttime = @timed @select(df, cols(:c) = testdotfun.(cols("a"), cols("b")))
-			slowtime = @timed select(df, [:a, :b] => ((a, b) -> testdotfun.(a, b)) => :c)
-			@test slowtime.time > fasttime.time
+				fasttime = @timed @select(df, cols(:c) = testdotfun.(cols("a"), cols("b")))
+				slowtime = @timed select(df, [:a, :b] => ((a, b) -> testdotfun.(a, b)) => :c)
+				@test slowtime[2] > fasttime[2]
 
-		gd = groupby(df, :a)
+			gd = groupby(df, :a)
 
-		@test @combine(gd, testnt(:b)) == DataFrame(a = [1], c = [2])
+			@test @combine(gd, testnt(:b)) == DataFrame(a = [1], c = [2])
 
-			fasttime = @timed @combine(gd, testnt(:b))
-			slowtime = @timed combine(gd, :b => (b -> testnt(b)) => AsTable)
-			@test slowtime.time > fasttime.time
+				fasttime = @timed @combine(gd, testnt(:b))
+				slowtime = @timed combine(gd, :b => (b -> testnt(b)) => AsTable)
+				@test slowtime[2] > fasttime[2]
+		end
 	end
 end
 end # module
