becbf476a5ee688914ea2f4955f6971c2032b2b6
Peter Deffebach <p.deffebach@gmail.com>
2021-04-06 14:28:57 -0400

    fix debugging

--- src/parsing.jl
+++ src/parsing.jl
@@ -145,8 +145,6 @@ function fun_to_vec(ex::Expr; nolhs::Bool = false, gensym_names::Bool = false)
     # cols(:y) = f(cols(:x)) # re-write as simple call, RHS is block, use cols
     # cols(y) = :x + 1 # re-write as complicated col, but RHS is :block
     # cols(:y) = cols(:x) + 1 # re-write as complicated call, RHS is block, use cols
-    dump(ex)
-    asdf
     Base.remove_linenums!(ex)
 
     if gensym_names
