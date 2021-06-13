216dbe039017c5444d2f0923336f4eb57d784e3c
Peter Deffebach <p.deffebach@gmail.com>
2021-06-13 18:00:33 -0400

    fix tests

--- test/grouping.jl
+++ test/grouping.jl
@@ -379,8 +379,8 @@ end
 	@test @transform(g, t = :c).a ≅ d.a
 	@test @select(g, :a, t = :c).a ≅ d.a
 
-    @test @transform(g, t = @byrow :a ^ 2).t ≅ d.a .^ 2
-    @test @select(g, :a, t = @byrow :a ^ 2).t ≅ d.a .^ 2
+    @test @transform(g, @byrow t = :a ^ 2).t ≅ d.a .^ 2
+    @test @select(g, :a, @byrow t = :a ^ 2).t ≅ d.a .^ 2
 end
 
 @testset "@where with a grouped data frame" begin
