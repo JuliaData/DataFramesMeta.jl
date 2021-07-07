e8a21ef394e3890afd0cf501589284f8fac246bd
Peter Deffebach <p.deffebach@gmail.com>
2021-07-07 10:00:43 -0400

    update src/linq.jl

--- src/linqmacro.jl
+++ src/linqmacro.jl
@@ -9,6 +9,10 @@ export @linq, linq
 """
     @linq df ...
 
+
+!!! note
+    `@linq` is deprecated. Use `@chain` instead. See `? @chain` for details.
+
 General macro that creates a mini DSL for chaining and macro calls.
 
 ### Details
@@ -34,11 +38,11 @@ julia> df = DataFrame(
             b = repeat(2:-1:1, outer = 4),
             x = 1:8);
 
-julia> x1 = @linq transform(where(df, :a .> 2, :b .!= "c"), y = 10 .* :x);
+julia> x1 = @linq transform(where(df, :a .> 2, :b .!= "c"), :y = 10 .* :x);
 
-julia> x1 = @linq by(x1, :b, meanX = mean(:x), meanY = mean(:y));
+julia> x1 = @linq by(x1, :b, :meanX = mean(:x), :meanY = mean(:y));
 
-julia> @linq select(orderby(x1, :b, -:meanX), var = :b, :meanX, :meanY)
+julia> @linq select(orderby(x1, :b, -:meanX), :var = :b, :meanX, :meanY)
 2×3 DataFrame
 │ Row │ var   │ meanX   │ meanY   │
 │     │ Int64 │ Float64 │ Float64 │
@@ -49,7 +53,7 @@ julia> @linq select(orderby(x1, :b, -:meanX), var = :b, :meanX, :meanY)
 julia> @linq df |>
            transform(y = 10 .* :x) |>
            where(:a .> 2) |>
-           by(:b, meanX = mean(:x), meanY = mean(:y)) |>
+           by(:b, :meanX = mean(:x), :meanY = mean(:y)) |>
            orderby(:meanX) |>
            select(:meanX, :meanY, var = :b)
 2×3 DataFrame
