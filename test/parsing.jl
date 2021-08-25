9dd3f21ca84f7c45653d98482f1f8ec1d552cf4e
Peter Deffebach <p.deffebach@gmail.com>
2021-08-25 15:41:47 -0400

    add test set for get column expression

---
+++ test/parsing.jl
@@ -0,0 +1,58 @@
+module TestParsing
+
+using Test
+using DataFramesMeta
+using Statistics
+
+const ≅ = isequal
+
+import DataFramesMeta: get_column_expr
+
+@testset "Returning columnn identifiers" begin
+    macro protect(x)
+        esc(get_column_expr(x))
+    end
+
+    v_sym = @protect $[:a, :b]
+    @test v_sym == [:a, :b]
+
+    v_str = @protect $["a", "b"]
+    @test v_str == ["a", "b"]
+
+    qn = @protect :x
+    @test qn == :x
+
+    qn_protect = @protect $:x
+    @test qn_protect == :x
+
+    str_protect = @protect $"x"
+    @test qn_protect == "x"
+
+    x = "a"
+    sym_protect = @protect $x
+    @test sym_protect === "a"
+
+    v = ["a", "b"]
+    v_protect = @protect $v
+    @test v === v_protect
+
+    b = @protect $(Between(:a, :b))
+    @test b == Between(:a, :b)
+
+    i = @protect $1
+    @test i == 1
+
+    n = @protect "hello"
+    @test n === nothing
+
+    n = @protect 1
+    @test n === nothing
+
+    n = @protect begin x end
+    @test n === nothing
+
+    n = @protect x
+    @test n === nothing
+end
+
+end # module
