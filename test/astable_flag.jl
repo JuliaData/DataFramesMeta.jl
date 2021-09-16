b77e8ca9161ae308a3092e1ce24d03184b7b646c
Peter Deffebach <p.deffebach@gmail.com>
2021-09-16 04:58:56 -0400

    more tests

--- test/astable_flag.jl
+++ test/astable_flag.jl
@@ -1,22 +1,123 @@
 module TestAsTableFlag
 
 using Test
-using DataFrames
 using DataFramesMeta
 using Statistics
 
 const ≅ = isequal
 
-@testset "@astable macro flag" begin
+@testset "@astable with just assignments" begin
     df = DataFrame(a = 1, b = 2)
 
     d = @rtransform df @astable begin
         :x = 1
-        y = 50
-        :a = :x + y
+
+        nothing
     end
 
-    @test d == DataFrame(a = 51, b = 2, x = 1)
+    @test d == DataFrame(a = 1, b = 2, x = 1)
+
+    d = @rselect df @astable begin
+        :x = 1
+        y = 100
+        nothing
+    end
+
+    @test d == DataFrame(x = 1)
+
+    d = @transform df @astable begin
+        :x = [5]
+        y = 100
+        nothing
+    end
+
+    @test d == DataFrame(a = 1, b = 2, x = 5)
+
+    d = @select df @astable begin
+        :x = [5]
+        y = 100
+        nothing
+    end
+
+    @test d == DataFrame(x = 5)
+end
+
+@testset "@astable with just assignments, mutating" begin
+    # After finalizing above testset
+end
+
+@testset "@astable with strings" begin
+    df = DataFrame(a = 1, b = 2)
+
+    x_str = "x"
+    d = @rtransform df @astable begin
+        $x_str = 1
+        y = 100
+        nothing
+    end
+
+    @test d == DataFrame(a = 1, b = 2, x = 1)
+
+    d = @rselect df @astable begin
+        $x_str = 1
+        y = 100
+        nothing
+    end
+
+    @test d == DataFrame(x = 1)
+
+    d = @transform df @astable begin
+        $x_str = [5]
+        y = 100
+        nothing
+    end
+
+    @test d == DataFrame(a = 1, b = 2, x = 5)
+
+    d = @select df @astable begin
+        $x_str = [5]
+        y = 100
+        nothing
+    end
+
+    @test d == DataFrame(x = 5)
+end
+
+@testset "Re-using variables" begin
+    df = DataFrame(a = 1, b = 2)
+
+    d = @rtransform df @astable begin
+        :x = 1
+        y = 5
+        :z = :x + y
+    end
+
+    @test d == DataFrame(a = 1, b = 2, x = 1, z = 6)
+
+    d = @rselect df @astable begin
+        :x = 1
+        y = 5
+        :z = :x + y
+    end
+
+    @test d == DataFrame(x = 1, z = 6)
+
+    x_str = "x"
+    d = @rtransform df @astable begin
+        $x_str = 1
+        y = 5
+        :z = $x_str + y
+    end
+
+    @test d == DataFrame(a = 1, b = 2, x = 1, z = 6)
+
+    d = @rselect df @astable begin
+        $x_str = 1
+        y = 5
+        :z = $x_str + y
+    end
+
+    @test d == DataFrame(x = 1, z = 6)
 end
 
 
