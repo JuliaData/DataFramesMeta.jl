4f7f2328eb5f8230c0cabe1278eda3d387b00c12
Peter Deffebach <p.deffebach@gmail.com>
2022-01-30 11:36:57 -0500

    list keyword arguments

--- test/keyword_arguments.jl
+++ test/keyword_arguments.jl
@@ -8,35 +8,55 @@ using Statistics
 const ≅ = isequal
 
 # @subset
+# skipmissing, view, ungroup
 
 # @rsubset
+# skipmissing, view, ungroup
 
 # @subset!
+# skipmissing, view, ungroup
 
 # @rsubset!
+# skipmissing, view, ungroup
 
 # @orderby # Not added
 
 # @rorderby # Not added
 
 # @select
+# copycols, renamecols (not relevant)
+# keepkeys, ungroup
 
 # @rselect
+# copycols, renamecols (not relevant)
+# keepkeys, ungroup
 
 # @select!
+# renamecols (not relevant), ungroup
 
 # @rselect!
+# renamecols (not relevant), ungroup
 
 # @transform
+# copycols, renamecols (not relevant)
+# ungroup
 
 # @rtransform
+# copycols, renamecols (not relevant)
+# ungroup
 
 # @transform!
+# renamecols (not relevant), ungroup
 
 # @rtransform!
+# renamecols (not relevant), ungroup
 
 # @combine
+# renamecols (not relevant), keepkeys,
+# ungroup
 
 # @by
+# renamecols (not relevant), keepkeys,
+# ungroup
 
 end # module
