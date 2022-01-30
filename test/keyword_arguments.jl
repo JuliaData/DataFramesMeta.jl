fe6e01c4620b2d7256d3cc03a948ab20cc7f8240
Peter Deffebach <p.deffebach@gmail.com>
2022-01-30 11:23:59 -0500

    outline of tests

---
+++ test/keyword_arguments.jl
@@ -0,0 +1,42 @@
+module TestKW
+
+using Test
+using DataFrames
+using DataFramesMeta
+using Statistics
+
+const ≅ = isequal
+
+# @subset
+
+# @rsubset
+
+# @subset!
+
+# @rsubset!
+
+# @orderby # Not added
+
+# @rorderby # Not added
+
+# @select
+
+# @rselect
+
+# @select!
+
+# @rselect!
+
+# @transform
+
+# @rtransform
+
+# @transform!
+
+# @rtransform!
+
+# @combine
+
+# @by
+
+end # module
