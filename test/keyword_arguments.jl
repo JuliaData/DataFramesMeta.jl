b33de786a7602104f9b0478183135938e35bc931
Peter Deffebach <p.deffebach@gmail.com>
2022-09-24 15:26:07 -0400

    tests for subset

--- test/keyword_arguments.jl
+++ test/keyword_arguments.jl
@@ -7,17 +7,36 @@ using Statistics
 
 const ≅ = isequal
 
+df = DataFrame(a = [1, 1, 2, 2], b = [3, 4, 5, missing])
+gd = groupby(df, :a)
+
 # @subset
 # skipmissing, view, ungroup
+df2 = @subset(df, :a .== 1; view = true)
+@test df2 ≅ view(df, df.a .== 1, :)
+@test_throws ArgumentError @subset(df, :b .== 3; skipmissing = false)
+gd2 = @subset(gd, [true, true]; ungroup = false)
+@test gd2 ≅ groupby(df, :a)
 
 # @rsubset
 # skipmissing, view, ungroup
+df2 = @rsubset(df, :a == 1; view = true)
+@test df2 ≅ view(df, df.a .== 1, :)
+@test_throws ArgumentError @rsubset(df, :b == 3; skipmissing = false)
+gd2 = @rsubset(gd, first(true); ungroup = false)
+@test gd2 ≅ groupby(df, :a)
 
 # @subset!
-# skipmissing, view, ungroup
+# skipmissing, ungroup
+@test_throws ArgumentError @subset!(copy(df), :b .== 3; skipmissing = false)
+gd2 = @subset!(deepcopy(gd), [true, true]; ungroup = false)
+@test gd2 ≅ groupby(df, :a)
 
 # @rsubset!
-# skipmissing, view, ungroup
+# skipmissing, ungroup
+@test_throws ArgumentError @rsubset!(copy(df), :b == 3; skipmissing = false)
+gd2 = @rsubset!(deepcopy(gd), first(true); ungroup = false)
+@test gd2 ≅ groupby(df, :a)
 
 # @orderby # Not added
 
