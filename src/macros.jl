ab6bc1be8ae9d30bc90956084d5084b2102e2837
Peter Deffebach <p.deffebach@gmail.com>
2022-10-14 10:36:27 -0400

    add docstring to kwargs

--- src/macros.jl
+++ src/macros.jl
@@ -499,6 +499,32 @@ macro astable(args...)
     throw(ArgumentError("@astable only works inside DataFramesMeta macros."))
 end
 
+"""
+    @kwarg(args...)
+
+Inside of DataFramesMeta.jl macros, pass keyword arguments to the underlying
+DataFrames.jl function when arguments are written in "block" format.
+
+```
+julia> df = DataFrame(x = [1, 1, 2, 2], b = [5, 6, 7, 8]);
+
+julia> @rsubset df begin
+           :x == 1
+           @kwarg view = true
+       end
+2×2 SubDataFrame
+ Row │ x      b
+     │ Int64  Int64
+─────┼──────────────
+   1 │     1      5
+   2 │     1      6
+```
+
+!!!
+    This only has meaning inside DataFramesMeta.jl macros. It does not work outside
+    of DataFrames.jl macros.
+
+"""
 macro kwarg(args...)
     throw(ArgumentError("@kwarg only works inside DataFramesMeta macros."))
 end
