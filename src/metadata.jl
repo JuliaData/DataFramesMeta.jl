c4a17367ec6a28b00a3a699799c454aafa86a839
Peter Deffebach <p.deffebach@gmail.com>
2024-02-27 11:47:20 -0500

    more docs

--- src/metadata.jl
+++ src/metadata.jl
@@ -227,6 +227,29 @@ Print the notes and labels in a data frame.
 For the purposes of printing, column labels are printed in
 addition to notes. However column labels are not returned by
 `note(df, col)`.
+
+```
+julia> df = DataFrame(wage = [12], age = [23]);
+
+julia> @label! df :age = "Age (years)";
+
+julia> @note! df :wage = "Derived from American Community Survey";
+
+julia> @note! df :wage = "Missing values imputed as 0 wage";
+
+julia> @label! df :wage = "Hourly wage (2015 USD)";
+
+julia> printnotes(df)
+Column: wage
+────────────
+Label: Hourly wage (2015 USD)
+Derived from American Community Survey
+Missing values imputed as 0 wage
+
+Column: age
+───────────
+Label: Age (years)
+```
 """
 function printnotes(df, cols = All(); unnoted = false)
     nms = names(df, cols)
