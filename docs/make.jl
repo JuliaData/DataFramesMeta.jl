3bc7d7b4ca758dc64490be4315406c4315b23327
abhro <5664668+abhro@users.noreply.github.com>
2025-05-26 15:19:43 -0400

    Retab docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,15 +1,16 @@
 using Documenter, DataFramesMeta
 
 makedocs(
-	modules = [DataFramesMeta],
-	sitename = "DataFramesMeta Documentation",
-	format = Documenter.HTML(
-		canonical = "https://juliadata.github.io/DataFramesMeta.jl/stable/"
-	),
-	pages = Any[
-		"Introduction" => "index.md",
-		"Tutorial for coming from dplyr" => "dplyr.md",
-		"API" => "api/api.md"])
+    modules = [DataFramesMeta],
+    sitename = "DataFramesMeta Documentation",
+    format = Documenter.HTML(
+        canonical = "https://juliadata.github.io/DataFramesMeta.jl/stable/"
+    ),
+    pages = Any[
+        "Introduction" => "index.md",
+        "Tutorial for coming from dplyr" => "dplyr.md",
+        "API" => "api/api.md"],
+)
 
 deploydocs(
     repo = "github.com/JuliaData/DataFramesMeta.jl.git",
